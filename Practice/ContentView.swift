2abe77d474aa8620af0ac94d371b926d26c5abce
yadav-deepakk <deepakyadav53882@gmail.com>
2026-09-04 18:43:10 +0530

    Initial Commit

---
+++ Practice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Practice
+//
+//  Created by Deepak Kumar Yadav on 04/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Practice/PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeApp.swift
+//  Practice
+//
+//  Created by Deepak Kumar Yadav on 04/09/26.
+//
+
+import SwiftUI
+
+@main
+struct PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
