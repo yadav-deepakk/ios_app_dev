5cfe5811c7dbd464ed249d66e895fc9732e32ab3
yadav-deepakk <deepakyadav53882@gmail.com>
2026-09-18 13:01:41 +0530

    project1: wesplit app done.

--- Practice/ContentView.swift
+++ Practice/ContentView.swift
@@ -8,31 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var amountFieldVal: String = "0.00"
+    @State private var peopleCount: Int = 2
+    @State private var tipPercentage: Int = 20
+    @FocusState private var amountFieldFocus: Bool
+    var percentageList = [5,10,15,20,0]
     
-    @Environment(AuthStore.self) private var authStore: AuthStore
+    var finalTotalPerPerson : Double {
+        debugPrint("Amount:\(amountFieldVal), People:\(peopleCount),  Tip:\(tipPercentage)")
+        let a = (Double(amountFieldVal) ?? 0.0)
+        let tip = Double(tipPercentage)
+        let people = Double(peopleCount)
+        debugPrint("total: \(a+(a*tip/100)), people: \(people)")
+        debugPrint("total per person: \( (a + (a*tip/100) ) / people)")
+        return (a + (a*tip/100) ) / people
+    }
     
     var body: some View {
-        NavigationStack{
-            VStack{
-                Text("username is = \(authStore.username)")
-                TextField("username", text: Binding(
-                    get: { authStore.username },
-                    set: { authStore.username = $0 }
-                ))
-                    .padding()
-                    .textInputAutocapitalization(.never)
-                    .background(Color(hue: 0,saturation: 0,brightness: 0,opacity: 0.05))
-                    .cornerRadius(10)
-                NavigationLink("second view", destination: HomeView())
-                
-            }.padding()
-            .navigationTitle(Text("Env object"))
-        }
         
+        NavigationStack(){
+            Form{
+                Section{
+                    TextField("Enter Amount", text: $amountFieldVal)
+                        .keyboardType(.numberPad)
+                        .focused($amountFieldFocus)
+                    Picker("People:", selection: $peopleCount){
+                        ForEach(2...40, id: \.self) {
+                            Text("\($0) People")
+                        }
+                    }
+                }
+                Section("Tip Percentage"){
+                    Picker("Tip Percentage", selection: $tipPercentage){
+                        ForEach(percentageList, id: \.self) {
+                            Text("\(Int($0))%")
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                Section("Final Amount(Per Person)"){
+                    Text("\(finalTotalPerPerson)")
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar(){
+                Button("Done"){
+                    self.amountFieldFocus = false
+                }
+            }
+        }
     }
 }
 
 #Preview {
     ContentView()
-        .environment(AuthStore())
 }
