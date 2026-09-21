c9443fef65f6201c8a1143596d024d4ed71a4788
yadav-deepakk <deepakyadav53882@gmail.com>
2026-09-21 23:37:13 +0530

    Day23: Views and modiefiers

--- Practice/ContentView.swift
+++ Practice/ContentView.swift
@@ -7,11 +7,45 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct CapsuleModifier: ViewModifier {
+    var bgColor: Color?
+    func body(content: Content) -> some View {
+        content
+            .padding(.vertical, 10)
+            .padding(.horizontal, 10)
+            .foregroundStyle(.white)
+            .background(bgColor ?? .green)
+            .clipShape(.capsule)
+            .shadow(radius: 4)
+    }
+}
+
+extension View {
+    func capsuleStyle(bgColor: Color? = nil) -> some View {
+        modifier(CapsuleModifier(bgColor: bgColor))
+    }
+}
+
+struct CapsuleButton : View {
+    var text: String
+    var bgColor: Color?
+    var action: () -> Void
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-            Text("Hello World!")
+        Button(action: self.action, label: {
+            Text(text.capitalized)
+                .capsuleStyle(bgColor: bgColor)
+        })
+    }
+}
+
+struct ContentView: View {
+    @State private var count: Int = 0
+    var body : some View {
+        Text("\(count)")
+            .font(.largeTitle.bold())
+        HStack{
+            CapsuleButton(text: "UP") { count += 1 }
+            CapsuleButton(text: "DOWN", bgColor: .red) { count -= 1 }
         }
     }
 }
